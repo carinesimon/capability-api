750e8abbf699af21d318f2c8f43c888c58236135
carinesimon <contact@capability.fr>
2025-10-28 23:09:14 +0100

    Create metrics.controller.ts

---
+++ src/modules/metrics/metrics.controller.ts
@@ -0,0 +1,15 @@
+import { Controller, Get, Query } from '@nestjs/common';
+import { MetricsService } from './metrics.service';
+
+@Controller('metrics')
+export class MetricsController {
+  constructor(private metrics: MetricsService) {}
+
+  @Get('funnel')
+  async getFunnel(@Query('start') start: string, @Query('end') end: string) {
+    const s = new Date(start);
+    const e = new Date(end);
+    const totals = await this.metrics.funnelTotals({ start: s, end: e });
+    return { totals };
+  }
+}
