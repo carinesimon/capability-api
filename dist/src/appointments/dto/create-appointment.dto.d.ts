c339a724a402f9ce42656df168bfd1b7f321ba29
carinesimon <contact@capability.fr>
2025-10-29 00:08:06 +0100

    Add files via upload

--- dist/src/appointments/dto/create-appointment.dto.d.ts
+++ dist/src/appointments/dto/create-appointment.dto.d.ts
@@ -1,1 +1,9 @@
-
+import { AppointmentStatus, AppointmentType } from '@prisma/client';
+export declare class CreateAppointmentDto {
+    type: AppointmentType;
+    status: AppointmentStatus;
+    scheduledAt: string;
+    leadId: string;
+    userId: string;
+    notes?: string;
+}
