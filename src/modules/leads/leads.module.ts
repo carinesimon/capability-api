881094146442fdc7e03c7ed038e524f927f441b7
carinesimon <contact@capability.fr>
2025-11-02 11:07:41 +0100

    Update leads.module.ts

--- src/modules/leads/leads.module.ts
+++ src/modules/leads/leads.module.ts
@@ -1,13 +1,17 @@
 // src/modules/leads/leads.module.ts
 import { Module } from '@nestjs/common';
 import { PrismaModule } from '../../prisma/prisma.module';
-import { StageEventsService } from './stage-events.service';
-
 import { LeadsService } from './leads.service';
+import { StageEventsService } from './stage-events.service';
+import { LeadsController } from './leads.controller';
 
 @Module({
   imports: [PrismaModule],
+  controllers: [LeadsController],
   providers: [LeadsService, StageEventsService],
-  exports: [LeadsService, StageEventsService],
+  exports: [
+    LeadsService,
+    StageEventsService, // 👈 on l’exporte pour que les autres modules puissent l’injecter
+  ],
 })
 export class LeadsModule {}
