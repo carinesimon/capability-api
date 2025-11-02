cda2860e6c8a2e044b410f5d50373c5186798d20
carinesimon <contact@capability.fr>
2025-11-02 11:08:48 +0100

    Update ghl.module.ts

--- src/integrations/ghl/ghl.module.ts
+++ src/integrations/ghl/ghl.module.ts
@@ -2,8 +2,13 @@ import { Module } from '@nestjs/common';
 import { GhlController } from './ghl.controller';
 import { GhlService } from './ghl.service';
 import { PrismaService } from '../../prisma/prisma.service';
-
+import { PrismaModule } from '../../prisma/prisma.module';
+import { LeadsModule } from '../../modules/leads/leads.module';
 @Module({
+    imports: [
+    PrismaModule,
+    LeadsModule, // 👈 pour injecter StageEventsService dans GhlService
+  ],
   controllers: [GhlController],
   providers: [GhlService, PrismaService],
 })
