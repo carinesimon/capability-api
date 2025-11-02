77669da8bbc81c074cacf370721425097940cce1
carinesimon <contact@capability.fr>
2025-11-02 11:08:17 +0100

    Update integrations.module.ts

--- src/integrations/integrations.module.ts
+++ src/integrations/integrations.module.ts
@@ -4,9 +4,9 @@ import { IntegrationsService } from './integrations.service';
 import { IntegrationsController } from './integrations.controller';
 import { HookController } from './hook.controller';
 import { AutoAssignService } from './auto-assign.service';
-
+import { LeadsModule } from '../modules/leads/leads.module';
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, LeadsModule,],
   providers: [IntegrationsService, AutoAssignService],
   controllers: [IntegrationsController, HookController],
   exports: [IntegrationsService, AutoAssignService],
