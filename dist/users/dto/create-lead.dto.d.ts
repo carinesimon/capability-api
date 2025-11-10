03d4fb5e08edb3ee15da77b84a893d20c8731e55
carinesimon <contact@capability.fr>
2025-11-10 20:08:20 +0100

    Delete dist/users directory

--- dist/users/dto/create-lead.dto.d.ts
+++
@@ -1,7 +0,0 @@
-export declare class CreateLeadDto {
-    firstName: string;
-    lastName?: string;
-    email: string;
-    tag?: string;
-    assignedTo?: string;
-}
--- dist/users/dto/create-user.dto.d.ts
+++
@@ -1,7 +0,0 @@
-import { Role } from '@prisma/client';
-export declare class CreateUserDto {
-    firstName: string;
-    lastName?: string;
-    email: string;
-    role: Role;
-}
