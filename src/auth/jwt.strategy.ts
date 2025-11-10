90a9956b6a6f223007a3420f4b49323d666088d8
carinesimon <contact@capability.fr>
2025-11-10 23:00:28 +0100

    Update jwt.strategy.ts

--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -16,4 +16,6 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
     // payload = { sub, email, role, iat, exp }
     return { userId: payload.sub, email: payload.email, role: payload.role };
   }
+  
 }
+
