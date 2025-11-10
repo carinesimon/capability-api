8dfd9bbaa881cd754b2d3f1378bee4870dbcd4e2
carinesimon <contact@capability.fr>
2025-11-10 20:06:21 +0100

    Delete dist/budget directory

--- dist/budget/dto/create-budget.dto.d.ts
+++
@@ -1,7 +0,0 @@
-import { BudgetPeriod } from '@prisma/client';
-export declare class CreateBudgetDto {
-    period: BudgetPeriod;
-    amount: number;
-    weekStart?: string;
-    monthStart?: string;
-}
