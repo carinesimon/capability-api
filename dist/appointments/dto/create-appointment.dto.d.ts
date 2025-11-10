1fcb3d72630416cc99a0e3effa86838febcf2388
carinesimon <contact@capability.fr>
2025-11-10 20:05:12 +0100

    Delete dist/appointments directory

--- dist/appointments/dto/create-appointment.dto.d.ts
+++
@@ -1,9 +0,0 @@
-import { AppointmentStatus, AppointmentType } from '@prisma/client';
-export declare class CreateAppointmentDto {
-    type: AppointmentType;
-    status: AppointmentStatus;
-    scheduledAt: string;
-    leadId: string;
-    userId: string;
-    notes?: string;
-}
