201d2b985ed420f9aa4cc57e8c396db3c352c56b
carinesimon <contact@capability.fr>
2025-11-10 20:07:02 +0100

    Delete dist/integrations directory

--- dist/integrations/ghl/ghl.mapping.d.ts
+++
@@ -1,1 +0,0 @@
-export declare function mapGhlStageToLeadStage(ghlStageName?: string): "UNASSIGNED" | "RV0" | "RV1" | "RV2" | "WON" | "LOST" | "NOT_QUALIFIED" | null;
--- dist/integrations/hook.controller.d.ts
+++
@@ -1,9 +0,0 @@
-import { IntegrationsService } from './integrations.service';
-export declare class HookController {
-    private readonly svc;
-    constructor(svc: IntegrationsService);
-    receiveHook(routeKey: string, req: any, headers: Record<string, any>): Promise<{
-        ok: boolean;
-        eventId: string;
-    }>;
-}
