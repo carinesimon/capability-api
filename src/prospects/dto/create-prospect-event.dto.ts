12df7313a46906c9d4bcf9eaba104a2431e7b70e
carinesimon <contact@capability.fr>
2025-11-21 06:53:12 +0100

    Update create-prospect-event.dto.ts

--- src/prospects/dto/create-prospect-event.dto.ts
+++ src/prospects/dto/create-prospect-event.dto.ts
@@ -5,16 +5,20 @@ export enum StageDto {
   DEMANDE_APPEL = "DEMANDE_APPEL",
   APPEL_PASSE = "APPEL_PASSE",
   APPEL_REPONDU = "APPEL_REPONDU",
+  
   NO_SHOW_SETTER = "NO_SHOW_SETTER",
   RV0_PLANIFIE = "RV0_PLANIFIE",
   RV0_HONORE = "RV0_HONORE",
   RV0_NO_SHOW = "RV0_NO_SHOW",
+  
   RV1_PLANIFIE = "RV1_PLANIFIE",
   RV1_HONORE = "RV1_HONORE",
   RV1_NO_SHOW = "RV1_NO_SHOW",
+  
   RV2_PLANIFIE = "RV2_PLANIFIE",
   RV2_HONORE = "RV2_HONORE",
   RV2_NO_SHOW = "RV2_NO_SHOW",
+  
   RV0_ANNULE = "RV0_CANCELED",
   RV1_ANNULE = "RV1_CANCELED",
   RV2_ANNULE = "RV2_CANCELED",
@@ -79,3 +83,4 @@ export function normalizeStage(input?: string): StageDto | undefined {
   };
   return map[k];
 }
+
