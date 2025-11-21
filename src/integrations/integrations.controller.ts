e34f79b3fa05e6f62bf12857e4c3bfa51b96c6c1
carinesimon <contact@capability.fr>
2025-11-21 06:54:56 +0100

    Update integrations.controller.ts

--- src/integrations/integrations.controller.ts
+++ src/integrations/integrations.controller.ts
@@ -2,7 +2,6 @@ import { Body, Controller, Delete, Get, Param, Patch, Post, Query } from '@nestj
 import { IntegrationsService } from './integrations.service';
 import { LeadStage } from '@prisma/client';
 
-
 @Controller('integrations')
 export class IntegrationsController {
   constructor(private readonly svc: IntegrationsService) {}
@@ -70,3 +69,4 @@ async get(@Param('id') id: string) {
     return this.svc.replayEvent(eventId);
   }
 }
+
